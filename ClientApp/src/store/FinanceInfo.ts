b8c3b0ad9ad8a58ffba8e61e283eea4ededb6eb4
Benjamin Lehmann <bclehmann@yahoo.ca>
2019-12-31 23:06:29 -0700

    Dropdown tickers

--- ClientApp/src/store/FinanceInfo.ts
+++ ClientApp/src/store/FinanceInfo.ts
@@ -4,16 +4,34 @@ import API from "../api";
 
 type BasicAction = { type: string } & Action;
 type FetchInfoAction = { tickerSymbol: string } & BasicAction;
-type ReceiveResultsAction = { results: any[] } & BasicAction;
+type ReceiveResultsAction = { results: resultsType } & BasicAction;
+
+interface resultsType {
+  bestMatches: Array<any>;
+}
+
+interface matchType {
+  symbol: string;
+  name: string;
+  type: string;
+  region: string;
+  marketOpen: string;
+  marketClose: string;
+  timezone: string;
+  currency: string;
+  matchscore: number;
+}
 
 const initialState = {
   tickerSymbol: undefined,
-  results: undefined
+  results: {
+    bestMatches: new Array()
+  }
 };
 
 export interface IState {
   tickerSymbol?: string;
-  results?: any;
+  results: resultsType;
 }
 
 const actionTypes = {
@@ -26,8 +44,8 @@ export const actionCreators = (dispatch: any) => ({
     dispatch({ type: actionTypes.fetchInfo, tickerSymbol } as BasicAction);
   },
   searchTickers: async (tickerSymbol: string) => {
-    var searchResultsPromise = API.searchSymbols(tickerSymbol);
-    var results = await searchResultsPromise;
+    var searchResultsPromise = await API.searchSymbols(tickerSymbol);
+    var results = JSON.parse(searchResultsPromise.data);
     dispatch({ type: actionTypes.receiveResults, results } as BasicAction);
   }
 });
@@ -46,7 +64,27 @@ export const reducer: Reducer<IState> = (
       return { ...state, tickerSymbol: fetchAction.tickerSymbol };
     case actionTypes.receiveResults:
       let receiveAction = incomingAction as ReceiveResultsAction;
-      return { ...state, results: receiveAction.results };
+      //@ts-ignore
+      if (receiveAction.results["Error Message"] !== undefined) {
+        return {
+          ...state,
+          results: initialState.results
+        };
+      }
+      let results = receiveAction.results;
+      results.bestMatches= results.bestMatches.map(m => ({
+        //The API is actually low-key stupid
+        symbol: m["1. symbol"],
+        name: m["2. name"],
+        type: m["3. type"],
+        region: m["4. region"],
+        marketOpen: m["5. marketOpen"],
+        marketClose: m["6. marketClose"],
+        timezone: m["7. timezone"],
+        currency: m["8. currency"],
+        matchScore: m["9. matchScore"]
+      }));
+      return { ...state, results };
     default:
       return state;
   }
